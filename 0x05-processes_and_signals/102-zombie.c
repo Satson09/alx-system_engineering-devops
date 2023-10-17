8037e6becc37dc4f4e9ce102ded8aad4a4d7db95
satson09 <satsunjohnson@gmail.com>
2023-10-17 14:09:04 +0100

    0x05. Processes and signals be re-study

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,3 +1,28 @@
+/*
+ * File: 102-zombie.c
+ * Auth: Satson Johnson
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Run an infinite while loop.
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+
 /**
  * main - Creates five zombie processes.
  *
