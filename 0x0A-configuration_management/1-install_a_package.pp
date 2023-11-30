6a13895e36a7cf22c54f706078c0b5c7fd0fdf3f
satson09 <satsunjohnson@gmail.com>
2023-11-30 02:52:00 +0000

    Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install flask from pip3
-package { 'flask':
+# Install a specific version of flask (2.1.0) from pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  } 
+  provider => 'pip3'
+}
